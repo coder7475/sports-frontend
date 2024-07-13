b3df69af972f78c93c9b6442fc75ffbc1a2f625e
coder7475 <robiulhossain7475@gmail.com>
2024-07-13 23:52:51 +0600

    feat: add a footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/footer/Footer";
 import NavBar from "./components/navbar/Navbar";
 
 const App = () => {
@@ -5,6 +6,9 @@ const App = () => {
     <main className="font-helve container mx-auto mt-3">
       <section className="max-w-[1280px] mx-auto">
         <NavBar />
+      </section>
+      <section className="max-w-[1280px] mx-auto">
+        <Footer />
       </section>
     </main>
   );
