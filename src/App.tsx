2758eeecba579a91a61d8bfb3c5f40c3065eb7e9
coder7475 <robiulhossain7475@gmail.com>
2024-07-13 20:43:12 +0600

    feat: set the container

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { Button } from "@/components/ui/button";
 
 const App = () => {
   return (
-    <main className="font-helve max-w-[1280px] mx-auto mt-3">
-      <nav className="flex justify-between items-center text-base">
-        <h2>accelPro</h2>
-        <ul className="flex justify-between w-[60%] items-center">
-          <li>lorem</li>
-          <li>jipsum</li>
-          <li>sajf</li>
-          <li>dsafdf</li>
-          <Button>Click me</Button>
-        </ul>
-      </nav>
+    <main className="font-helve container mx-auto mt-3">
+      <section className="max-w-[1280px] mx-auto">
+        <nav className="flex justify-between items-center text-base">
+          <h2>accelPro</h2>
+          <ul className="flex justify-between w-[60%] items-center">
+            <li>lorem</li>
+            <li>jipsum</li>
+            <li>sajf</li>
+            <li>dsafdf</li>
+            <Button>Click me</Button>
+          </ul>
+        </nav>
+      </section>
     </main>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,6 @@ module.exports = {
   prefix: "",
   theme: {
     fontFamily: { helve: ["Helvetica", "sans-serif"] },
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
