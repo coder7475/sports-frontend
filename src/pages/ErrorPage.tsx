8343dceac0d2e35e4abc7ebe6aaf01f114304e03
coder7475 <robiulhossain7475@gmail.com>
2024-07-17 19:37:06 +0600

    feat: add a basic error page

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,20 @@
+import { useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div
+      id="error-page"
+      className="flex flex-col gap-5 justify-center items-center h-screen"
+    >
+      <h1 className="text-5xl text-center font-black">Oops!</h1>
+      <p className="text-center text-xl font-semibold">
+        Sorry, an unexpected error has occurred.
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
