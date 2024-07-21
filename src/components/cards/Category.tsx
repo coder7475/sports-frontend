c63499119648390dd28c977e9ad420c1b230ab28
coder7475 <robiulhossain7475@gmail.com>
2024-07-21 15:01:03 +0600

    feat: create top categories UI

---
+++ src/components/cards/Category.tsx
@@ -0,0 +1,14 @@
+const Category = () => {
+  return (
+    <button className="rounded-lg relative text-white font-bold text-2xl hover:scale-90 hover:text-3xl">
+      <img
+        src="/src/assets/fdgdfg.jpeg"
+        alt=""
+        className="brightness-50 aspect-square rounded-lg object-fill"
+      />
+      <h3 className="absolute top-1/2 left-1/2">Shoes</h3>
+    </button>
+  );
+};
+
+export default Category;
--- src/pages/homepage/TopCategories.tsx
+++ src/pages/homepage/TopCategories.tsx
@@ -1,9 +1,16 @@
+import Category from "@/components/cards/Category";
+
 const TopCategories = () => {
   return (
-    <div>
+    <div className="">
       <h2 className="text-5xl font-extrabold h-36 flex justify-center items-center">
         Top Categories
       </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        <Category />
+        <Category />
+        <Category />
+      </div>
     </div>
   );
 };
