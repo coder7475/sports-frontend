3b40f61b7dd745a48146d9545ded15400af127c2
coder7475 <robiulhossain7475@gmail.com>
2024-07-27 22:58:52 +0600

    feat: change the navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,32 +1,39 @@
 // A navbar with logo and necessary menu items (e.g. All Products, Manage Products, Cart, About Us)
 import { Button } from "@/components/ui/button";
+import { FaCircle } from "react-icons/fa6";
 
 const NavBar = () => {
   return (
-    <nav className="flex justify-between items-center text-base">
-      <h2 className="text-3xl font-bold tracking-wider">
-        <a href="/">
-          Accel<span className="text-primary">P</span>ro
+    <header className="text-gray-400 bg-gray-900 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a
+          className="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+          href="/"
+        >
+          <FaCircle className="fill-primary text-xl" />
+          <span className="ml-3 text-2xl md:text-3xl font-bold tracking-wider">
+            Accel<span className="text-primary">P</span>ro
+          </span>
         </a>
-      </h2>
-      <ul className="flex justify-center gap-20 items-center text-[16px]">
-        <li className="hover:font-bold">
-          <a href="/">Home</a>
-        </li>
-        <li className="hover:font-bold">
-          <a href="/">All Products</a>
-        </li>
-        <li className="hover:font-bold">
-          <a href="/manage-products">Manage Products</a>
-        </li>
-        <li className="hover:font-bold">
-          <a href="/about">About Us</a>
-        </li>
-      </ul>
-      <Button>
-        <CartIcon />
-      </Button>
-    </nav>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+          {/* <a className="mr-5 hover:text-primary" href="/">
+            Home
+          </a> */}
+          <a className="mr-5 hover:text-primary" href="/">
+            All Products
+          </a>
+          <a className="mr-5 hover:text-primary" href="/manage-products">
+            Manage Products
+          </a>
+          <a className="mr-5 hover:text-primary" href="/about">
+            About Us
+          </a>
+        </nav>
+        <Button className="mt-2">
+          <CartIcon />
+        </Button>
+      </div>
+    </header>
   );
 };
 
