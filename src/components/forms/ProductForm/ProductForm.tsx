ff945f915a9b48d58fc99cdcfb277ee3a003a381
coder7475 <robiulhossain7475@gmail.com>
2024-08-05 22:15:51 +0600

    feat: resutructure first 2 input field of add form

--- src/components/forms/ProductForm/ProductForm.tsx
+++ src/components/forms/ProductForm/ProductForm.tsx
@@ -29,44 +29,46 @@ const ProductForm = () => {
       onSubmit={handleSubmit(onSubmit)}
       className="mx-auto rounded-lg mt-8 space-y-6 bg-card w-full"
     >
-      <div className="max-w-md mx-auto">
-        <label
-          htmlFor="name"
-          className="block text-start text-lg font-semibold text-card-foreground"
-        >
-          Product Name{" "}
-          <input
-            type="text"
-            id="name"
-            {...register("name", { required: "Product Name is required" })}
-            className="mt-1 text-black px-1 py-1 block w-full rounded-md bg-card-foreground border-primary focus:border-primary"
-          />
-        </label>
-        {errors.name && (
-          <Alert variant="destructive">
-            <AlertDescription>{errors.name.message}</AlertDescription>
-          </Alert>
-        )}
-      </div>
+      <div className="flex flex-col w-full lg:flex-row gap-4 px-8 pt-8">
+        <div className="w-full">
+          <label
+            htmlFor="name"
+            className="block text-start text-lg font-semibold text-card-foreground"
+          >
+            Product Name{" "}
+            <input
+              type="text"
+              id="name"
+              {...register("name", { required: "Product Name is required" })}
+              className="mt-1 text-black px-1 py-1 block w-full rounded-md bg-card-foreground border-primary focus:border-primary"
+            />
+          </label>
+          {errors.name && (
+            <Alert variant="destructive">
+              <AlertDescription>{errors.name.message}</AlertDescription>
+            </Alert>
+          )}
+        </div>
 
-      <div>
-        <label
-          htmlFor="brand"
-          className="block text-base font-medium text-card-foreground"
-        >
-          Brand
-        </label>
-        <input
-          type="text"
-          id="brand"
-          {...register("brand", { required: "Brand is required" })}
-          className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-        />
-        {errors.brand && (
-          <Alert variant="destructive">
-            <AlertDescription>{errors.brand.message}</AlertDescription>
-          </Alert>
-        )}
+        <div className="w-full">
+          <label
+            htmlFor="brand"
+            className="block text-start text-lg font-semibold text-card-foreground"
+          >
+            Brand{" "}
+            <input
+              type="text"
+              id="brand"
+              {...register("brand", { required: "Brand is required" })}
+              className="mt-1 text-black px-1 py-1 block w-full rounded-md bg-card-foreground border-primary focus:border-primary"
+            />
+          </label>
+          {errors.brand && (
+            <Alert variant="destructive">
+              <AlertDescription>{errors.brand.message}</AlertDescription>
+            </Alert>
+          )}
+        </div>
       </div>
 
       <div>
