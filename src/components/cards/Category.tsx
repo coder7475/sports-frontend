252e40f4a7c521fe0e0536300f99c9ceabeaef29
coder7475 <robiulhossain7475@gmail.com>
2024-08-31 22:39:16 +0600

    feat: redesign top categories

--- src/components/cards/Category.tsx
+++ src/components/cards/Category.tsx
@@ -1,12 +1,12 @@
-const Category = () => {
+const Category = ({ img, name }) => {
   return (
     <button className="rounded-lg relative text-white font-bold text-2xl hover:scale-90 hover:text-3xl">
       <img
-        src="/src/assets/fdgdfg.jpeg"
+        src={img}
         alt=""
         className="brightness-50 aspect-square rounded-lg object-fill"
       />
-      <h3 className="absolute top-1/2 left-1/2">Shoes</h3>
+      <h3 className="absolute top-1/2 left-0 right-0">{name}</h3>
     </button>
   );
 };
--- src/pages/homepage/TopCategories.tsx
+++ src/pages/homepage/TopCategories.tsx
@@ -7,9 +7,24 @@ const TopCategories = () => {
         Top Categories
       </h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-        <Category />
-        <Category />
-        <Category />
+        <Category
+          img={
+            "https://boltsfitness.com/images/resource/71AyxR0yeeL._SL1500_.jpg"
+          }
+          name={"Fitness"}
+        />
+        <Category
+          img={
+            "https://www.freevector.com/uploads/vector/preview/8374/FreeVector-Ball-Sports.jpg"
+          }
+          name={"Ball Sports"}
+        />
+        <Category
+          img={
+            "https://www.siroko.com/blog/c/app/uploads/2021/07/efectos-positivos_f766ef96-0811-452d-bbf8-ec38d244637a-1440x900.jpg.webp"
+          }
+          name={"Cycling"}
+        />
       </div>
     </div>
   );
