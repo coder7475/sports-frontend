d942e7b4624c52d1b31c6e5e19e55f1cca79d2ec
coder7475 <robiulhossain7475@gmail.com>
2024-09-06 12:10:42 +0600

    feat: update the category component to redirect all component page

--- src/components/cards/Category.tsx
+++ src/components/cards/Category.tsx
@@ -1,9 +1,20 @@
-const Category = ({ img, name }) => {
+import { useNavigate } from "react-router-dom";
+
+const Category = ({ img, name }: { img: string; name: string }) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/all-products?category=${encodeURIComponent(name)}`);
+  };
+
   return (
-    <button className="rounded-lg relative text-white font-bold text-2xl hover:scale-90 hover:text-3xl">
+    <button
+      className="rounded-lg relative text-white font-bold text-2xl hover:scale-90 hover:text-3xl"
+      onClick={handleClick}
+    >
       <img
         src={img}
-        alt=""
+        alt={name}
         className="brightness-50 aspect-square rounded-lg object-fill"
       />
       <h3 className="absolute top-1/2 left-0 right-0">{name}</h3>
